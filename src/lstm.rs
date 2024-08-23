4f0cbe194973c896285d10f5c23005e48ae809b7
Antoine Groudiev <antoine.groudiev@ens.psl.eu>
2024-08-23 12:26:51 +0200

    Continued LSTM

--- src/lstm.rs
+++ src/lstm.rs
@@ -1,5 +1,6 @@
 use nalgebra::DMatrix;
 use rand::prelude::SliceRandom;
+use rand::rngs::ThreadRng;
 use rand_distr::Normal;
 use std::collections::{HashMap, HashSet};
 use std::fs;
@@ -22,6 +23,31 @@ impl GateModel {
     fn new(w: DMatrix<f64>, u: DMatrix<f64>, b: DMatrix<f64>) -> Self {
         Self {
             parameters: [w, u, b],
+        }
+    }
+
+    fn from_distribution(
+        vocab_size: usize,
+        hidden_size: usize,
+        normal: &Normal<f64>,
+        rng: &mut ThreadRng,
+    ) -> Self {
+        Self {
+            parameters: [
+                0.01 * DMatrix::from_distribution_generic(
+                    nalgebra::Dyn(hidden_size),
+                    nalgebra::Dyn(vocab_size),
+                    &normal,
+                    rng,
+                ),
+                0.01 * DMatrix::from_distribution_generic(
+                    nalgebra::Dyn(hidden_size),
+                    nalgebra::Dyn(hidden_size),
+                    &normal,
+                    rng,
+                ),
+                DMatrix::zeros(hidden_size, 1),
+            ],
         }
     }
 
@@ -73,6 +99,30 @@ impl Model {
         }
     }
 
+    fn new_random(vocab_size: usize, hidden_size: usize) -> Self {
+        let mut rng = rand::thread_rng();
+        let normal = Normal::new(0.0, 1.0).unwrap();
+        Self {
+            parameters: [
+                GateModel::from_distribution(vocab_size, hidden_size, &normal, &mut rng),
+                GateModel::from_distribution(vocab_size, hidden_size, &normal, &mut rng),
+                GateModel::from_distribution(vocab_size, hidden_size, &normal, &mut rng),
+                GateModel::from_distribution(vocab_size, hidden_size, &normal, &mut rng),
+            ],
+        }
+    }
+
+    fn new_memory(vocab_size: usize, hidden_size: usize) -> Self {
+        Self {
+            parameters: [
+                GateModel::new_memory(vocab_size, hidden_size),
+                GateModel::new_memory(vocab_size, hidden_size),
+                GateModel::new_memory(vocab_size, hidden_size),
+                GateModel::new_memory(vocab_size, hidden_size),
+            ],
+        }
+    }
+
     fn update_memory(&mut self, gradient: &Model) {
         for i in 0..4 {
             self.parameters[i].update_memory(&gradient.parameters[i]);
@@ -81,7 +131,120 @@ impl Model {
 
     fn update_parameters(&mut self, learning_rate: f64, gradient: &Model, memory: &Model) {
         for i in 0..4 {
-            self.parameters[i].update_parameters(learning_rate, &gradient.parameters[i], &memory.parameters[i]);
+            self.parameters[i].update_parameters(
+                learning_rate,
+                &gradient.parameters[i],
+                &memory.parameters[i],
+            );
         }
     }
+
+    fn f(&self) -> &GateModel {
+        &self.parameters[0]
+    }
+
+    fn i(&self) -> &GateModel {
+        &self.parameters[1]
+    }
+
+    fn o(&self) -> &GateModel {
+        &self.parameters[2]
+    }
+
+    fn g(&self) -> &GateModel {
+        &self.parameters[3]
+    }
+}
+
+pub fn start(path: &String) {
+    /* File loading */
+    let data = fs::read_to_string(path).unwrap_or_else(|_| panic!("Cannot find file {path}"));
+    let chars: HashSet<char> = data.chars().collect();
+    let ix_to_char: HashMap<ix, char> = chars.clone().into_iter().enumerate().collect();
+    let char_to_ix: HashMap<char, ix> = chars
+        .clone()
+        .into_iter()
+        .enumerate()
+        .map(|(i, c)| (c, i))
+        .collect();
+    let vocab_size = chars.len();
+
+    println!("Data has {} characters, {} unique", data.len(), chars.len());
+
+    // Model weights and biases
+    let mut model = Model::new_random(vocab_size, HIDDEN_SIZE);
+
+    // Memory variables for AdaGrad
+    let mut memory = Model::new_memory(vocab_size, HIDDEN_SIZE);
+
+    let mut n = 0;
+    let mut p = 0;
+    let mut smooth_loss = (vocab_size as f64).ln() * SEQ_LENGTH as f64;
+    let mut prev_h = DMatrix::zeros(HIDDEN_SIZE, 1);
+    let mut prev_c = DMatrix::zeros(HIDDEN_SIZE, 1);
+
+    loop {
+        // truncated backpropagation through time
+        if p + SEQ_LENGTH + 1 >= data.len() || n == 0 {
+            // reset the hidden state
+            prev_h = DMatrix::zeros(HIDDEN_SIZE, 1);
+            prev_c = DMatrix::zeros(HIDDEN_SIZE, 1);
+            p = 0;
+        }
+
+        let inputs: Vec<ix> = data[p..(p + SEQ_LENGTH)]
+            .chars()
+            .map(|c| char_to_ix[&c])
+            .collect();
+        let targets: Vec<ix> = data[(p + 1)..(p + SEQ_LENGTH + 1)]
+            .chars()
+            .map(|c| char_to_ix[&c])
+            .collect();
+
+        // sample from the model
+        if n % SAMPLE_INTERVAL == 0 {
+            let sample = sample(&prev_h, &prev_c, inputs[0], SAMPLE_SIZE, vocab_size, &model);
+            let txt: String = sample.iter().map(|i| ix_to_char[i]).collect();
+            println!("----\n {} \n----", txt);
+        }
+
+        // Retrieve loss and gradient
+        let (loss, gradient, new_prev_h, new_prev_c) =
+            loss_function(inputs, targets, &prev_h, &prev_c, vocab_size, &model);
+        prev_h = new_prev_h;
+        prev_c = new_prev_c;
+        smooth_loss = smooth_loss * 0.999 + loss * 0.001;
+        if n % SAMPLE_INTERVAL == 0 {
+            println!("iteration {}, loss: {}", n, smooth_loss);
+        }
+
+        // Update parameters with AdaGrad
+        memory.update_memory(&gradient);
+        model.update_parameters(LEARNING_RATE, &gradient, &memory);
+
+        p += SEQ_LENGTH;
+        n += 1;
+    }
+}
+
+fn loss_function(
+    inputs: Vec<usize>,
+    targets: Vec<usize>,
+    prev_h: &DMatrix<f64>,
+    prev_c: &DMatrix<f64>,
+    vocab_size: usize,
+    model: &Model,
+) -> (f64, Model, DMatrix<f64>, DMatrix<f64>) {
+    unimplemented!()
+}
+
+fn sample(
+    h: &DMatrix<f64>,
+    c: &DMatrix<f64>,
+    seed_letter: ix,
+    n: usize,
+    vocab_size: usize,
+    model: &Model,
+) -> Vec<ix> {
+    unimplemented!()
 }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,21 @@ mod rnn;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if args.len() != 3 {
         println!("usage: rnn-text-gen <architecture> <filepath>\n\narchitectures:\n    rnn \tvanilla Recurrent Neural Network\n    lstm\tLong Short-Term Memory");
         std::process::exit(1);
     }
-    let path = &args[1];
+    let architecture = &args[1];
+    let path = &args[2];
+
+    match architecture.as_str() {
+        "rnn" => rnn::start(path),
+        "lstm" => lstm::start(path),
+        _ => {
+            println!("usage: rnn-text-gen <architecture> <filepath>\n\narchitectures:\n    rnn \tvanilla Recurrent Neural Network\n    lstm\tLong Short-Term Memory");
+            std::process::exit(1);
+        }
+    }
 
     rnn::start(path);
 }
